dfc00fe4826fd1036bc921ac46d0e694c009753d
FW0912 <wongso.felix@gmail.com>
2025-12-30 23:18:39 +0700

    Update server route to redirect base path to home page

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from "@angular/ssr";
 
 export const serverRoutes: ServerRoute[] = [
 	{
+		path: "",
+		renderMode: RenderMode.Server,
+	},
+	{
 		path: "view-plan/:id",
 		renderMode: RenderMode.Server,
 	},
